339e6b680a48109b2dffcd15a0401e93e08c413b
Owais Idris <owaisforandroid@gmail.com>
2023-03-31 22:48:51 +0530

    Published Common to local maven. Official way

--- common/src/main/java/com/artsman/common/ModuleVerifier.kt
+++ common/src/main/java/com/artsman/common/ModuleVerifier.kt
@@ -1,7 +1,15 @@
 package com.artsman.common
 
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
 class ModuleVerifier {
     operator fun invoke(): Boolean{
+        val result = Json.encodeToString(Sample())
+        print("Json Serialization: $result")
         return true
     }
 }
+
+@kotlinx.serialization.Serializable
+data class Sample(val name: String= "XYZ")
